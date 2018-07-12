58041d8c87fadc9888f0ed1dee308c54445f2409
Alex Clark <aclark@aclark.net>
2018-07-12 12:26:38 -0400

    Update

--- aclark/root/static/index.js
+++ aclark/root/static/index.js
@@ -1,3 +1,6 @@
 import 'bootstrap';
 import 'bootstrap-table';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './dist/toolkit.min.js'
+import './docs/assets/css/application-startup.css';
+import './docs/assets/css/toolkit-startup.css';
