0984d0da4d8b0c1a52267ae521ec5313f8309575
Alex Clark <aclark@aclark.net>
2018-10-08 10:22:51 -0400

    Update

--- aclark/db/choices.py
+++ aclark/db/choices.py
@@ -49,3 +49,28 @@ ESTIMATE_TYPES = (
     ('is_sow', 'Statement of Work'),
     ('is_to', 'Task Order'),
 )
+
+# Not really a choice, but close enough.
+
+URL_NAMES = {
+    'Settings App': ('settings_app', 'settings_app_edit', ''),
+    'Settings Company': ('settings_company', 'settings_company_edit', ''),
+    'Settings Contract': ('settings_contract', 'settings_contract_edit', ''),
+    'client': ('client_view', 'client_edit', 'client_index'),
+    'contact': ('contact_view', 'contact_edit', 'contact_index'),
+    'contract': ('contract_view', 'contract_edit', 'contract_index'),
+    'estimate': ('estimate_view', 'estimate_edit', 'estimate_index'),
+    'file': ('file_view', 'file_edit', 'file_index'),
+    'invoice': ('invoice_view', 'invoice_edit', 'invoice_index'),
+    'log': ('log_view', 'log_edit', 'log_index'),
+    'newsletter': ('newsletter_view', 'newsletter_edit', 'newsletter_index'),
+    'note': ('note_view', 'note_edit', 'note_index'),
+    'profile': ('user_view', 'user_edit', 'user_index'),
+    'project': ('project_view', 'project_edit', 'project_index'),
+    'proposal': ('proposal_view', 'proposal_edit', 'proposal_index'),
+    'report': ('report_view', 'report_edit', 'report_index'),
+    'service': ('', 'service_edit', ''),
+    'task': ('task_view', 'task_edit', 'task_index'),
+    'time': ('time_view', 'time_edit', 'time_index'),
+    'user': ('user_view', 'user_edit', 'user_index'),
+}
--- aclark/db/obj.py
+++ aclark/db/obj.py
@@ -2,29 +2,7 @@ from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.utils import timezone
-
-URL_NAMES = {
-    'Settings App': ('settings_app', 'settings_app_edit', ''),
-    'Settings Company': ('settings_company', 'settings_company_edit', ''),
-    'Settings Contract': ('settings_contract', 'settings_contract_edit', ''),
-    'client': ('client_view', 'client_edit', 'client_index'),
-    'contact': ('contact_view', 'contact_edit', 'contact_index'),
-    'contract': ('contract_view', 'contract_edit', 'contract_index'),
-    'estimate': ('estimate_view', 'estimate_edit', 'estimate_index'),
-    'file': ('file_view', 'file_edit', 'file_index'),
-    'invoice': ('invoice_view', 'invoice_edit', 'invoice_index'),
-    'log': ('log_view', 'log_edit', 'log_index'),
-    'newsletter': ('newsletter_view', 'newsletter_edit', 'newsletter_index'),
-    'note': ('note_view', 'note_edit', 'note_index'),
-    'profile': ('user_view', 'user_edit', 'user_index'),
-    'project': ('project_view', 'project_edit', 'project_index'),
-    'proposal': ('proposal_view', 'proposal_edit', 'proposal_index'),
-    'report': ('report_view', 'report_edit', 'report_index'),
-    'service': ('', 'service_edit', ''),
-    'task': ('task_view', 'task_edit', 'task_index'),
-    'time': ('time_view', 'time_edit', 'time_index'),
-    'user': ('user_view', 'user_edit', 'user_index'),
-}
+from . import choices
 
 
 def obj_process(obj,
@@ -45,20 +23,20 @@ def obj_process(obj,
         http_ref = request.META.get('HTTP_REFERER')
     if task == 'url':
         if page_type == 'view':
-            url_name = URL_NAMES[model_name][0]
+            url_name = choices.URL_NAMES[model_name][0]
             template_name = '%s.html' % url_name
             return template_name, url_name
         elif page_type == 'copy':
-            url_name = URL_NAMES[model_name][1]
+            url_name = choices.URL_NAMES[model_name][1]
             return url_name
         elif page_type == 'edit':
-            template_name = '%s.html' % URL_NAMES[model_name][1]
+            template_name = '%s.html' % choices.URL_NAMES[model_name][1]
             return template_name
         elif page_type == 'dashboard':
             url_name = 'dashboard'
             return url_name
         elif page_type == 'index':
-            url_name = URL_NAMES[model_name][2]
+            url_name = choices.URL_NAMES[model_name][2]
             return url_name
     elif task == 'copy':
         dup = obj
