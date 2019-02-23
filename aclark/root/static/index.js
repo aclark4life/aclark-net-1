00c6fac1e5d50c10c9a2867ddfa873ef5c783782
Alex Clark <aclark@aclark.net>
2019-02-23 18:11:47 -0500

    Update

--- aclark/root/static/index.js
+++ aclark/root/static/index.js
@@ -5,4 +5,3 @@ import './db.css';
 import './dist/toolkit-startup.min.css';
 import './docs/assets/css/application-startup.css';
 import './docs/assets/js/application.js';
-import 'thelounge/index.js';
