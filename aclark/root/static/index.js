5cc9efd7d45f6af8fefcc5477fd542e9240b5194
Alex Clark <aclark@aclark.net>
2019-06-28 22:06:05 -0400

    Update

--- aclark/root/static/index.js
+++ aclark/root/static/index.js
@@ -7,11 +7,15 @@ import './dashboard.css';
 import './dist/toolkit-startup.min.css';
 import './docs/assets/css/application-startup.css';
 import './signin.css';
+import 'bootstrap-table/dist/bootstrap-table.min.css';
+
 import './aclark.js';
 import './docs/assets/js/application.js';
 import './docs/assets/js/toolkit.js';
 import './dashboard.js';
 import 'jquery/dist/jquery.js';
 import 'popper.js/dist/popper.js';
+import 'bootstrap-table/dist/bootstrap-table.min.js';
+
 
 // import './dashboard.js';
