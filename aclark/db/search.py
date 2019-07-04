9d73515d1646cc95e2ea440ff53cd4a5270ba5fa
Alex Clark <aclark@aclark.net>
2019-07-04 18:30:25 -0400

    Update

---
+++ aclark/db/search.py
@@ -0,0 +1,29 @@
+from django.db.models import Q
+from operator import or_ as OR
+from functools import reduce
+from .misc import set_items
+
+
+def get_search_results(
+    context,
+    model,
+    search_fields,
+    search,
+    edit_url=None,
+    view_url=None,
+    order_by=None,
+    request=None,
+):
+    query = []
+    model_name = model._meta.verbose_name
+    for field in search_fields:
+        query.append(Q(**{field + "__icontains": search}))
+    items = model.objects.filter(reduce(OR, query))
+    context["%s_nav" % model_name] = True
+    context["edit_url"] = edit_url
+    context["view_url"] = view_url
+    if order_by is not None:
+        items = items.order_by(*order_by)
+    items = set_items(model_name, items=items)
+    context["items"] = items
+    return context
