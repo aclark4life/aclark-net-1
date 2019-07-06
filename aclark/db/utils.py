bfa36ca265f03887cf9b41084ad7e4e92dd374c3
Alex Clark <aclark@aclark.net>
2019-07-06 09:26:13 -0400

    Update

--- aclark/db/utils.py
+++ aclark/db/utils.py
@@ -94,7 +94,6 @@ def get_page_items(**kwargs):
     time_model = kwargs.get("time_model")
 
     model = kwargs.get("model")
-    obj = kwargs.get("obj")
     pk = kwargs.get("pk")
 
     filter_by = kwargs.get("filter_by")
@@ -106,44 +105,32 @@ def get_page_items(**kwargs):
 
     request = kwargs.get("request")
 
+    doc = get_query_string(request, "doc")
+    mail = get_query_string(request, "mail")
+    pdf = get_query_string(request, "pdf")
+
+    net = 0
+    gross = 0
+    hours = 0
+    cost = 0
+
     context = {}
-    items = {}
-
-    if request:  # Applies to all page items
-        doc = get_query_string(request, "doc")  # Export
-        mail = get_query_string(request, "mail")  # Export
-        pdf = get_query_string(request, "pdf")  # Export
-        context["doc"] = doc
-        context["mail"] = mail
-        context["pdf"] = pdf
-        context["request"] = request  # Include request
-
+    items = []
+    item = None
     model_name = None
+    fields = None
 
-    if model or obj:
-        if model:
-            model_name = model._meta.verbose_name
-        elif obj:
-            model_name = obj._meta.verbose_name
+    last_payment_date = None
 
-        context["model_name"] = model_name
-        context["%s_nav" % model_name] = True
-        context["edit_url"] = "%s_edit" % model_name
-        context["view_url"] = "%s_view" % model_name
-
-        if report_model:
-            reports = report_model.objects.filter(active=True).order_by("-date")
-            items = set_items("report", items=reports)
-
-        context["items"] = items
-
+    if model:
+        model_name = model._meta.verbose_name
         if model_name == "client":
-            client = get_object_or_404(model, pk=pk)
-            contacts = contact_model.objects.filter(client=client)
-            estimates = estimate_model.objects.filter(client=client)
-            invoices = invoice_model.objects.filter(client=client)
-            projects = project_model.objects.filter(client=client)
-            notes = note_model.objects.filter(client=client)
+            item = get_object_or_404(model, pk=pk)
+            contacts = contact_model.objects.filter(client=item)
+            estimates = estimate_model.objects.filter(client=item)
+            invoices = invoice_model.objects.filter(client=item)
+            projects = project_model.objects.filter(client=item)
+            notes = note_model.objects.filter(client=item)
             if order_by:
                 invoices = invoices.order_by(*order_by["invoice"])
                 projects = projects.order_by(*order_by["project"])
@@ -153,54 +140,39 @@ def get_page_items(**kwargs):
             items = set_items("note", items=notes, _items=items)
             items = set_items("project", items=projects, _items=items)
             items = set_items("estimate", items=estimates, _items=items)
-            context["item"] = client
         elif model_name == "contact":
-            contact = get_object_or_404(model, pk=pk)
+            item = get_object_or_404(model, pk=pk)
             fields = get_fields(
-                contact, include_fields=include_fields
+                item, include_fields=include_fields
             )  # fields_items.html
-            context["fields"] = fields
         elif model_name == "estimate":  # handle obj or model
-            if not obj:
-                estimate = get_object_or_404(model, pk=pk)
-            else:
-                estimate = obj
-            times = time_model.objects.filter(estimate=estimate)
+            item = get_object_or_404(model, pk=pk)
+            times = time_model.objects.filter(estimate=item)
             if order_by:
                 times = times.order_by(*order_by["time"])
-            times = set_total(times, estimate=estimate)
+            times = set_total(times, estimate=item)
             config = (
                 site_config_model.get_solo()
             )  # get_solo will create the item if it does not already exist
-            context["doc_type"] = model_name
-            context["config"] = config
-            context["entries"] = times
-            context["item"] = estimate
         elif model_name == "invoice":
-            invoice = get_object_or_404(model, pk=pk)
+            item = get_object_or_404(model, pk=pk)
             config = (
                 site_config_model.get_solo()
             )  # get_solo will create the item if it does not already exist
-            times = time_model.objects.filter(estimate=None, invoice=invoice)
+            times = time_model.objects.filter(estimate=None, invoice=item)
             times = times.order_by(*order_by["time"])
-            times = set_total(times, invoice=invoice)
-            last_payment_date = invoice.last_payment_date
-            context["doc_type"] = model_name
-            context["times"] = times
-            context["item"] = invoice
-            context["invoice"] = True
-            context["config"] = config
-            context["last_payment_date"] = last_payment_date
+            times = set_total(times, invoice=item)
+            last_payment_date = item.last_payment_date
         elif model_name == "project":
-            project = get_object_or_404(model, pk=pk)
+            item = get_object_or_404(model, pk=pk)
             contacts = contact_model.objects.all()
-            estimates = estimate_model.objects.filter(project=project)
-            invoices = invoice_model.objects.filter(project=project)
-            notes = note_model.objects.filter(project=project)
+            estimates = estimate_model.objects.filter(project=item)
+            invoices = invoice_model.objects.filter(project=item)
+            notes = note_model.objects.filter(project=item)
             times = time_model.objects.filter(
-                estimate=None, project=project, task__isnull=False, invoiced=False
+                estimate=None, project=item, task__isnull=False, invoiced=False
             )
-            times = set_total(times, project=project, time_model=time_model)
+            times = set_total(times, project=item, time_model=time_model)
             if order_by:
                 times = times.order_by(*order_by["time"])
                 invoices = invoices.order_by(*order_by["invoice"])
@@ -209,108 +181,98 @@ def get_page_items(**kwargs):
             items = set_items("invoice", items=invoices, _items=items)
             items = set_items("time", items=times, _items=items)
             items = set_items("note", items=notes, _items=items)
-            context["item"] = project
-            context["cost"] = float(project.cost)
-            context["gross"] = float(project.amount)
-            context["net"] = float(project.amount) - float(project.cost)
         elif model_name == "report":
-            report = get_object_or_404(model, pk=pk)
+            item = get_object_or_404(model, pk=pk)
             reports = model.objects.filter(active=True).order_by("-date")
             reports.aggregate(cost=Sum(F("cost")))
             reports.aggregate(gross=Sum(F("gross")))
             reports.aggregate(net=Sum(F("net")))
-            invoices = report.invoices.all()
+            invoices = item.invoices.all()
             items = set_items("invoice", items=invoices, _items=items)
             items = set_items("report", items=reports, _items=items)
-            context["item"] = report
             # E-Mail
             context["message"] = "Cost: %s, Gross: %s, Net: %s" % (
-                report.cost,
-                report.gross,
-                report.net,
+                item.cost,
+                item.gross,
+                item.net,
             )
-            context["subject"] = report.name
+            context["subject"] = item.name
         elif model_name == "task":
-            task = get_object_or_404(model, pk=pk)
-            context["item"] = task
-        if model_name == "time":
-            time = get_object_or_404(model, pk=pk)
-            context["item"] = time
+            item = get_object_or_404(model, pk=pk)
+        elif model_name == "time":
+            item = get_object_or_404(model, pk=pk)
             fields = get_fields(
-                time, include_fields=include_fields
+                item, include_fields=include_fields
             )  # fields_table.html
-            context["fields"] = fields  # fields_items.html
         elif model_name == "user":
-            user = get_object_or_404(model, pk=pk)
-            projects = project_model.objects.filter(team__in=[user], active=True)
+            item = get_object_or_404(model, pk=pk)
+            projects = project_model.objects.filter(team__in=[item], active=True)
             projects = projects.order_by(*order_by["project"])
-            times = time_model.objects.filter(estimate=None, invoiced=False, user=user)
+            times = time_model.objects.filter(estimate=None, invoiced=False, user=item)
             times = times.order_by(*order_by["time"])
             contacts = contact_model.objects.all()
             fields = get_fields(
-                user.profile, include_fields=include_fields
+                item.profile, include_fields=include_fields
             )  # fields_table.html
             hours = get_total("hours", times=times)
-            context["fields"] = fields
-            context["item"] = user
-            context["projects"] = projects
-            context["times"] = times
-            context["hours"] = hours
         elif model_name == "note":
-            note = get_object_or_404(model, pk=pk)
+            item = get_object_or_404(model, pk=pk)
             fields = get_fields(
-                note, include_fields=include_fields
+                item, include_fields=include_fields
             )  # fields_items.html
-            context["fields"] = fields
-            context["item"] = note
         else:
             item = get_object_or_404(model, pk=pk)
-            context["item"] = item
-    else:  # no model or obj
-        if request:
-            if request.user.is_authenticated:
-                # Items
-                invoices = invoice_model.objects.filter(last_payment_date=None)
-                invoices = invoices.order_by(*order_by["invoice"])
-                projects = project_model.objects.filter(active=True, hidden=False)
-                projects = projects.order_by(*order_by["project"])
-                if filter_by:
-                    times = time_model.objects.filter(**filter_by["time"])
-                else:
-                    times = time_model.objects.all()
-                times = times.order_by(*order_by["time"])
-                times = set_total(times)
-                items = set_items("invoice", items=invoices)
-                items = set_items("project", items=projects, _items=items)
-                items = set_items("time", items=times, _items=items)
-                # Paginate items
-                page_num = get_query_string(request, "page")
-                paginated = get_query_string(request, "paginated")
-                if paginated:  # Paginate if paginated
-                    page_size = get_setting(request, "page_size")
-                    if "times" in items:
-                        items["times"] = paginate(
-                            items["times"], page_num=page_num, page_size=page_size
-                        )
-                # Totals
-                net = 0
-                gross = get_total("gross", invoices=invoices)
-                hours = get_total("hours", times=times)
-                cost = get_total("cost", projects=projects)
-                if gross and cost:
-                    net = gross - cost
-                context["net"] = net
-                context["gross"] = gross
-                context["hours"] = hours
-                context["cost"] = cost
+    else:  # no model
+        # Items
+        invoices = invoice_model.objects.filter(last_payment_date=None)
+        invoices = invoices.order_by(*order_by["invoice"])
+        projects = project_model.objects.filter(active=True, hidden=False)
+        projects = projects.order_by(*order_by["project"])
+        if filter_by:
+            times = time_model.objects.filter(**filter_by["time"])
+        else:
+            times = time_model.objects.all()
+        times = times.order_by(*order_by["time"])
+        times = set_total(times)
+        items = set_items("invoice", items=invoices)
+        items = set_items("project", items=projects, _items=items)
+        items = set_items("time", items=times, _items=items)
+        # Paginate items
+        page_num = get_query_string(request, "page")
+        paginated = get_query_string(request, "paginated")
+        if paginated:  # Paginate if paginated
+            page_size = get_setting(request, "page_size")
+            if "times" in items:
+                items["times"] = paginate(
+                    items["times"], page_num=page_num, page_size=page_size
+                )
+        # Totals
 
-                # Location
-                ip_address = request.META.get("HTTP_X_REAL_IP")
-                context["ip_address"] = ip_address
+        gross = get_total("gross", invoices=invoices)
+        hours = get_total("hours", times=times)
+        cost = get_total("cost", projects=projects)
+        if gross and cost:
+            net = gross - cost
 
-                # Reports
-                reports = report_model.objects.filter(active=True).order_by("-date")
-                items = set_items("report", items=reports, _items=items)
-                context["items"] = items
+    context["item"] = item
+    context["net"] = net
+    context["gross"] = gross
+    context["hours"] = hours
+    context["cost"] = cost
+    context["items"] = items
+    context["model_name"] = model_name
+    context["%s_nav" % model_name] = True
+    context["edit_url"] = "%s_edit" % model_name
+    context["view_url"] = "%s_view" % model_name
+    context["doc"] = doc
+    context["mail"] = mail
+    context["pdf"] = pdf
+    context["request"] = request  # Include request
+    context["fields"] = fields
+    context["config"] = config
+    context["last_payment_date"] = last_payment_date
+
+    if report_model:
+        reports = report_model.objects.filter(active=True).order_by("-date")
 
     return context
