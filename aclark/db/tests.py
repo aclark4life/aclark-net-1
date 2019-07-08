f59e6dd7f934ab5894e35d9798dd22155e536024
Alex Clark <aclark@aclark.net>
2019-07-08 17:43:58 -0400

    Update

--- aclark/db/tests.py
+++ aclark/db/tests.py
@@ -1,13 +1,13 @@
 from django.test import TestCase
 from aclark.db.models import SiteConfiguration
+from aclark.db.models import BaseModel
+from aclark.db.models import Client
+from aclark.db.models import Contact
 
 # Create your tests here.
 
 from django.test import TestCase
 
-# class SiteConfiguration(SingletonModel):
-# class BaseModel(models.Model):
-# class Client(BaseModel):
 # class Contact(BaseModel):
 # class Estimate(BaseModel):
 # class Invoice(BaseModel):
@@ -29,3 +29,35 @@ class SiteConfigurationTestCase(TestCase):
         """
         self.assertEqual(self.config.company_name, "Company Name")
         self.assertEqual(self.config.site_name, "Site Name")
+
+
+class BaseModelTestCase(TestCase):
+    def setUp(self):
+        self.base = BaseModel()
+
+    def test_base(self):
+        """
+        """
+        self.assertTrue(self.base.active)
+        self.assertFalse(self.base.hidden)
+
+
+class ClientTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_client(self):
+        """
+        """
+        self.assertFalse(self.client.published)
+
+
+class ContactTestCase(TestCase):
+    def setUp(self):
+        self.contact = Contact(first_name="Alex", last_name="Clark")
+
+    def test_contact(self):
+        """
+        """
+        self.assertEqual(self.contact.first_name, "Alex")
+        self.assertEqual(self.contact.last_name, "Clark")
