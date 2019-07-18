e42a6b04f6c94e234ea65fa62ffb89f2556415d6
Alex Clark <aclark@aclark.net>
2019-07-18 12:38:05 -0400

    Update

--- aclark/db/views.py
+++ aclark/db/views.py
@@ -400,7 +400,7 @@ def project_index(request, pk=None):
     context = get_index_items(
         model=Project,
         report_model=Report,
-        order_by=("-active", "-updated"),
+        order_by=("-active", "-created"),
         request=request,
         search_fields=("id", "name"),
     )
