4f7b4e1356cb440ce58c5323d2c32a31d310396c
Alex Clark <aclark@aclark.net>
2019-08-01 13:43:05 -0400

    Update

--- aclark/root/static/index.js
+++ aclark/root/static/index.js
@@ -1,13 +1,13 @@
 //// import 'bootstrap';
 //// import 'bootstrap/dist/css/bootstrap.min.css';
 //import 'bootstrap-table/dist/bootstrap-table.min.css';
-//import './aclark.css';
+import './aclark.css';
 //import './carousel.css';
 //import './dashboard.css';
 //import './dist/toolkit-startup.css';
 //import './docs/assets/css/application-startup.css';
 //import './signin.css';
-//import './aclark.js';
+import './aclark.js';
 //import './docs/assets/js/application.js';
 //import './docs/assets/js/toolkit.js';
 //import './dashboard.js';
