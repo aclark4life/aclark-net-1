bde624954f1d60d54e9a452186aca1c7087a3c55
Alex Clark <aclark@aclark.net>
2019-08-01 13:32:10 -0400

    Update

--- aclark/root/static/index.js
+++ aclark/root/static/index.js
@@ -1,18 +1,18 @@
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-table/dist/bootstrap-table.min.css';
-import './aclark.css';
-import './carousel.css';
-import './dashboard.css';
-import './dist/toolkit-startup.css';
-import './docs/assets/css/application-startup.css';
-import './signin.css';
-import './aclark.js';
-import './docs/assets/js/application.js';
-import './docs/assets/js/toolkit.js';
-import './dashboard.js';
-import 'bootstrap-table/dist/bootstrap-table.min.js';
-import 'jquery/dist/jquery.js';
-import 'popper.js/dist/popper.js';
-
-// import './dashboard.js';
+//// import 'bootstrap';
+//// import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap-table/dist/bootstrap-table.min.css';
+//import './aclark.css';
+//import './carousel.css';
+//import './dashboard.css';
+//import './dist/toolkit-startup.css';
+//import './docs/assets/css/application-startup.css';
+//import './signin.css';
+//import './aclark.js';
+//import './docs/assets/js/application.js';
+//import './docs/assets/js/toolkit.js';
+//import './dashboard.js';
+//import 'bootstrap-table/dist/bootstrap-table.min.js';
+//import 'jquery/dist/jquery.js';
+//import 'popper.js/dist/popper.js';
+//
+//// import './dashboard.js';
