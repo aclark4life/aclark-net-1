e785763f46aee04016d07c20cb7e896e62eec4aa
Alex Clark <aclark@aclark.net>
2020-06-04 18:44:14 -0400

    Update

--- aclark/db/export.py
+++ aclark/db/export.py
@@ -8,6 +8,74 @@ from openpyxl import Workbook
 from openpyxl.styles import Font
 
 
+def render_xls_igce(context, **kwargs):
+    """
+    """
+
+    workbook = Workbook()
+    filename = kwargs.get("filename")
+    item = context["item"]
+    bold = Font(bold=True)
+
+    sheet1 = workbook.active
+    sheet1.title = "Instructions"
+    # https://stackoverflow.com/a/14450572
+    sheet1.append(["Instructions".upper()])
+    sheet1.column_dimensions["B"].width = 111.7
+    sheet1["A1"].font = bold
+    sheet1.merge_cells("A1:Z1")
+    sheet1.append(
+        [
+            "",
+            "This template is being provided as a tool to assist the acquisition workforce in developing an IGCE for a Firm Fixed Price Product or Service.",
+        ]
+    )
+    sheet1.append([])
+    sheet1.append(
+        [
+            "1.",
+            "The exact amount of a vendor’s quote must NOT be used as the basis for a program office’s IGCE.",
+        ]
+    )
+    sheet1.append(
+        [
+            "2.",
+            "The program office must conduct all necessary research to compile an accurate and complete IGCE, independent of the vendor’s pricing/cost information.",
+        ]
+    )
+    sheet1.append(
+        [
+            "3.",
+            "The program office should use the results of the market reaearch to substaniate the IGCE.",
+        ]
+    )
+    sheet1.append(
+        [
+            "4.",
+            "The program office provide a narrative to document the basis of the IGCE.",
+        ]
+    )
+    sheet2 = workbook.create_sheet(title="FFP IGCE")
+    sheet2.append(["FFP IGCE TEMPLATE"])
+    sheet2["A1"].font = bold
+    sheet2.column_dimensions["A"].width = 48
+    for entry in item.time_set.all():
+        sheet2.append(
+            [
+                entry.date,
+                entry.task.name,
+                entry.description,
+                entry.hours,
+                entry.task.rate,
+            ]
+        )
+
+    response = HttpResponse(content_type="xlsx")
+    response["Content-Disposition"] = "attachment; filename=%s" % filename
+    workbook.save(response)
+    return response
+
+
 def render_doc(context, **kwargs):
     """
     """
@@ -70,71 +138,6 @@ def render_xls(context, **kwargs):
     sheet1.title = "Invoice"
     for entry in item.time_set.all():
         sheet1.append(
-            [
-                entry.date,
-                entry.task.name,
-                entry.description,
-                entry.hours,
-                entry.task.rate,
-            ]
-        )
-
-    response = HttpResponse(content_type="xlsx")
-    response["Content-Disposition"] = "attachment; filename=%s" % filename
-    workbook.save(response)
-    return response
-
-
-def render_xls_igce(context, **kwargs):
-    """
-    """
-
-    workbook = Workbook()
-    filename = kwargs.get("filename")
-    item = context["item"]
-    bold = Font(bold=True)
-
-    sheet1 = workbook.active
-    sheet1.title = "Instructions"
-    # https://stackoverflow.com/a/14450572
-    sheet1.append(["Instructions".upper()])
-    sheet1.column_dimensions["B"].width = 111.7
-    sheet1["A1"].font = bold
-    sheet1.merge_cells("A1:Z1")
-    sheet1.append(
-        [
-            "",
-            "This template is being provided as a tool to assist the acquisition workforce in developing an IGCE for a Firm Fixed Price Product or Service.",
-        ]
-    )
-    sheet1.append([])
-    sheet1.append(
-        [
-            "1.",
-            "The exact amount of a vendor’s quote must NOT be used as the basis for a program office’s IGCE.",
-        ]
-    )
-    sheet1.append(
-        [
-            "2.",
-            "The program office must conduct all necessary research to compile an accurate and complete IGCE, independent of the vendor’s pricing/cost information.",
-        ]
-    )
-    sheet1.append(
-        [
-            "3.",
-            "The program office should use the results of the market reaearch to substaniate the IGCE.",
-        ]
-    )
-    sheet1.append(
-        [
-            "4.",
-            "The program office provide a narrative to document the basis of the IGCE.",
-        ]
-    )
-    sheet2 = workbook.create_sheet(title="FFP IGCE")
-    for entry in item.time_set.all():
-        sheet2.append(
             [
                 entry.date,
                 entry.task.name,
